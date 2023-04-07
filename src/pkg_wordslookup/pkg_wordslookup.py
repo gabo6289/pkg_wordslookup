8a717cefdb19c1d307b010f9ecc977a879819599
Gabriel Coronel <gabrielg.coronelm@gmail.com>
2023-04-07 15:19:57 +0200

    scr code updated

---
+++ src/pkg_wordslookup/pkg_wordslookup.py
@@ -0,0 +1,24 @@
+from string import punctuation
+def load_text(input_file):
+    """Load text from a text file and return it as a string"""
+    with open(input_file, "r") as file:
+        text = file.readlines()
+    return text
+
+def clean_text(text):
+    """Lowercase and remove punctuation from a string"""
+    text = text.lower()
+    for p in punctuation:
+        text = text.replace(p,"")
+    return text
+
+def words_lookup(input_file, input_word):
+    text = load_text(input_file)
+    n = 0
+    for line in text:
+        line = clean_text(line)
+        if input_word in line:
+            n+=1
+        else:
+            continue
+    print("The word \"{}\" appeared {} times".format(input_word,n))
